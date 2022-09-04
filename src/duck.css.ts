12bbb2f1c478b6f20c1310c519fcbf619cf1dc4c
PrettyCoffee <jonathan-holz@outlook.com>
2022-09-04 15:16:35 +0200

    feat: Show duck on low resolution

---
+++ src/duck.css.ts
@@ -0,0 +1,9 @@
+import { style } from "@vanilla-extract/css"
+
+export const duck = style({
+  position: "fixed",
+  width: "100%",
+  transform: "scale(1.5)",
+  inset: "0",
+  margin: "auto",
+})
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@
 import { render } from "solid-js/web"
 
 import { App } from "./app/App"
+import { Breakpoint } from "./components"
+import { duck } from "./duck.css"
 import { StoreProvider } from "./store/Store"
 import { ThemeProvider } from "./theme"
 
@@ -9,7 +11,12 @@ render(
   () => (
     <StoreProvider>
       <ThemeProvider>
-        <App />
+        <Breakpoint desktop laptop tablet>
+          <App />
+        </Breakpoint>
+        <Breakpoint mobile>
+          <img class={duck} src="assets/duck.gif" alt="" />
+        </Breakpoint>
       </ThemeProvider>
     </StoreProvider>
   ),
